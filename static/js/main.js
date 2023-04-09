243f8751d2a26656eaceac9a4574bee70a69c2aa
Harith Abeysinghe <e19003@eng.pdn.ac.lk>
2023-04-09 19:09:02 +0530

    Tried to add black and white pieces

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,7 @@ let parentDiv = document.querySelector("#board");
 
 let alphas = "abcdefgh".split("");
 let nums = "87654321".split("");
+
 const pieces = {
     'rook': 'static/img/pieces/Chess_rlt45.svg',
     'knight': 'static/img/pieces/Chess_nlt45.svg',
@@ -20,6 +21,68 @@ let moves = {
     'a7':'pawn','b7':'pawn','c7':'pawn','d7':'pawn','e7':'pawn','f7':'pawn','g7':'pawn','h7':'pawn',
     'a2':'pawn','b2':'pawn','c2':'pawn','d2':'pawn','e2':'pawn','f2':'pawn','g2':'pawn','h2':'pawn'
 }
+
+/*
+const pieces = {
+    'rook-white': {
+        image: 'static/img/pieces/Chess_rlt45.svg',
+        color: 'white'
+    },
+    'knight-white': {
+        image: 'static/img/pieces/Chess_nlt45.svg',
+        color: 'white'
+    },
+    'bishop-white': {
+        image: 'static/img/pieces/Chess_blt45.svg',
+        color: 'white'
+    },
+    'queen-white': {
+        image: 'static/img/pieces/Chess_qlt45.svg',
+        color: 'white'
+    },
+    'king-white': {
+        image: 'static/img/pieces/Chess_klt45.svg',
+        color: 'white'
+    },
+    'pawn-white': {
+        image: 'static/img/pieces/Chess_plt45.svg',
+        color: 'white'
+    },
+    'rook-black': {
+        image: 'static/img/pieces/Chess_rdt45.svg',
+        color: 'black'
+    },
+    'knight-black': {
+        image: 'static/img/pieces/Chess_ndt45.svg',
+        color: 'black'
+    },
+    'bishop-black': {
+        image: 'static/img/pieces/Chess_bdt45.svg',
+        color: 'black'
+    },
+    'queen-black': {
+        image: 'static/img/pieces/Chess_qdt45.svg',
+        color: 'black'
+    },
+    'king-black': {
+        image: 'static/img/pieces/Chess_kdt45.svg',
+        color: 'black'
+    },
+    'pawn-black': {
+        image: 'static/img/pieces/Chess_pdt45.svg',
+        color: 'black'
+    }
+};
+
+let moves = {
+    'a8':'rook-black','h8':'rook-black','a1':'rook-white','h1':'rook-white',
+    'b8':'knight-black','g8':'knight-black','b1':'knight-white','g1':'knight-white',
+    'c8':'bishop-black','f8':'bishop-black','c1':'bishop-white','f1':'bishop-white',
+    'd8':'queen-black','e8':'king-black','d1':'queen-white','e1':'king-white',
+    'a7':'pawn-black','b7':'pawn-black','c7':'pawn-black','d7':'pawn-black','e7':'pawn-black','f7':'pawn-black','g7':'pawn-black','h7':'pawn-black',
+    'a2':'pawn-white','b2':'pawn-white','c2':'pawn-white','d2':'pawn-white','e2':'pawn-white','f2':'pawn-white','g2':'pawn-white','h2':'pawn-white'
+}
+*/
 // create a table element
 
 //Add boader with alpha
@@ -72,6 +135,7 @@ document.body.appendChild(parentDiv);
 
 addboarder();
 
+
 function addboarder(){
     let divRow = document.createElement("div");
     divRow.setAttribute("class","div-row");
@@ -92,3 +156,5 @@ function addboarder(){
 
     parentDiv.appendChild(divRow);
 }
+
+  
